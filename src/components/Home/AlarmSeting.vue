02729d65be1255fb17b213d2e28fb919083b2a51
yujian <895144737@qq.com>
2019-05-24 09:32:27 +0800

    s

--- src/components/Home/AlarmSeting.vue
+++ src/components/Home/AlarmSeting.vue
@@ -6,32 +6,32 @@
       <img src='@/assets/icon/home/新增IC.png' class="add" @click='handleAddAlarm'/>
     </div>
     <div class="content">
-      <ul>
-        <li
-          class="alarm_item"
-          v-for='(item, index) in alarms'
-          :key='index'
-          @touchstart='touchstart(item, index)'
-          @touchend='touchend'>
-          <div class="info">
-            <p class="head">{{item.alarmlockDate}}</p>
-            <p class="desc">
-              <span>{{item.alarmclockRemarks|| '无'}}:</span>
-              <template v-if='item.date.length>1'>
-                <span v-if='item.date[0]=="1"' class="time">周日</span>
-                <span v-if='item.date[1]=="1"' class="time">周一</span>
-                <span v-if='item.date[2]=="1"' class="time">周二</span>
-                <span v-if='item.date[3]=="1"' class="time">周三</span>
-                <span v-if='item.date[4]=="1"' class="time">周四</span>
-                <span v-if='item.date[5]=="1"' class="time">周五</span>
-                <span v-if='item.date[6]=="1"' class="time">周六</span>
-              </template>
-              <span v-else class="title">{{item.date[0]}}</span>
-            </p>
-          </div>
-          <mt-switch size='mini' v-model="item.alarmclockStatus"></mt-switch>
-        </li>
-      </ul>
+        <ul>
+          <li
+            class="alarm_item"
+            v-for='(item, index) in alarms'
+            :key='index'
+            @touchstart='touchstart(item, index)'
+            @touchend='touchend'>
+            <div class="info">
+              <p class="head">{{item.alarmlockDate}}</p>
+              <p class="desc">
+                <span>{{item.alarmclockRemarks|| '无'}}:</span>
+                <template v-if='item.date.length>1'>
+                  <span v-if='item.date[0]=="1"' class="time">周日</span>
+                  <span v-if='item.date[1]=="1"' class="time">周一</span>
+                  <span v-if='item.date[2]=="1"' class="time">周二</span>
+                  <span v-if='item.date[3]=="1"' class="time">周三</span>
+                  <span v-if='item.date[4]=="1"' class="time">周四</span>
+                  <span v-if='item.date[5]=="1"' class="time">周五</span>
+                  <span v-if='item.date[6]=="1"' class="time">周六</span>
+                </template>
+                <span v-else class="title">{{item.date[0]}}</span>
+              </p>
+            </div>
+            <mt-switch @change='changeStatus(item, index)' :value="item.alarmclockStatus"></mt-switch>
+          </li>
+        </ul>
       <div class="action" ref='action'>
         <p class="editor" @click.stop='editorAlarm'>编辑</p>
         <p @click.stop='delAlarm'>删除</p>
@@ -39,12 +39,32 @@
     </div>
     <message-box
       v-if='isShowAddAlarm'
-      :title='title'
+      :title='title.add'
       @closeAddContact='addAlarm'
       class="add_black_list">
       <div class="add_black_list_wrapper">
         <p class="time"><label for="">时间</label><input v-model='alarmTime' @focus="selectTime" type="text"></p>
         <p class="title"><label for="">名称</label><input placeholder="" v-model='alarmName' class="name"/></p>
+        <div class="alarm">
+          <label for="">重复</label>
+          <div>
+            <span
+            v-for='(item, index) in date'
+            :key='index'
+            :class='{active:item.active}'
+            @click='item.active=!item.active'>{{item.label}}</span>
+          </div>
+        </div>
+      </div>
+    </message-box>
+    <message-box
+      v-if='isShowEditorAlarm'
+      :title='title.editor'
+      @closeAddContact='hideeditorAlarm'
+      class="add_black_list">
+      <div class="add_black_list_wrapper">
+        <p class="time"><label for="">时间</label><input v-model='selectAlarm.alarm.alarmlockDate' @focus="selectTime" type="text"></p>
+        <p class="title"><label for="">名称</label><input placeholder="" v-model='selectAlarm.alarm.alarmclockRemarks' class="name"/></p>
         <div class="alarm">
           <label for="">重复</label>
           <div>
@@ -71,19 +91,28 @@
 <script>
 import AddAlarm from '@/components/Home/AddAlarm'
 import MessageBox from '@/components/MessageBox'
+import Slider from '@/components/scroll'
 import { Switch, DatetimePicker, Toast } from 'mint-ui'
 export default {
   data () {
     return {
       time: 0,
       isShowAddAlarm: false,
-      title: '添加闹钟',
+      isShowEditorAlarm: false,
+      title: {
+        add: '新增闹钟',
+        editor: '修改闹钟'
+      },
       pickerVisible: '',
       alarmTime: '',
       alarmName: '',
       alarms: [
       ],
       date: [
+        {
+          label: '周天',
+          active: false
+        },
         {
           label: '周一',
           active: false
@@ -107,10 +136,6 @@ export default {
         {
           label: '周六',
           active: false
-        },
-        {
-          label: '周天',
-          active: false
         }
       ],
       selectAlarm: {
@@ -123,13 +148,18 @@ export default {
     AddAlarm,
     Switch,
     MessageBox,
-    DatetimePicker
+    DatetimePicker,
+    Slider
   },
   created () {
     this.$http.get(`${config.httpBaseUrl}/alar/getAll?wearerDeviceId=9611812844`).then(res => {
       if (res.code === 200) {
         res.date.alarmclock.forEach(item => {
-          // item.alarmclockStatus = item.alarmclockStatus && true
+          if (item.alarmclockStatus === 0) {
+            item.alarmclockStatus = false
+          } else {
+            item.alarmclockStatus = true
+          }
           if (item.alarmclockWeek == 1) {
             item.date = ['不重复']
           } else if (item.alarmclockWeek == 2) {
@@ -174,7 +204,6 @@ export default {
         } else if (num === 7) {
           alarmclockWeek = '2'
         }
-        console.log(alarmclockWeek)
         const data = {
           alarmlockDate: this.alarmTime,
           alarmclockWeek: alarmclockWeek,
@@ -184,8 +213,9 @@ export default {
           key: 'REMIND'
         }
         this.$http.post(`${config.httpBaseUrl}/alar/insert`, data).then(res => {
-          console.log(res)
-          this.alarms.push(data)
+          if (res.code === 200) {
+            this.alarms.push(data)
+          }
         })
         Toast({
           message: '操作成功',
@@ -211,6 +241,10 @@ export default {
     touchend (e) {
       clearTimeout(this.time)
     },
+    touchmove () {
+      
+      clearTimeout(this.time)
+    },
     // 展示操作窗口
     showAction(item, index) {
       this.selectAlarm.alarm = Object.assign({}, item)
@@ -225,8 +259,65 @@ export default {
     },
     // 修改闹钟
     editorAlarm () {
-      this.alarmTime = this.selectAlarm.alarm.alarmTime
-      this.alarmName = this.selectAlarm.alarm.alarmName
+      this.isShowEditorAlarm = true
+      if (this.selectAlarm.alarm.date[0] === '不重复') {
+        this.date.forEach(item => {
+          item.active = false
+        })
+      } else {
+        this.selectAlarm.alarm.date.forEach((item, index) => {
+          if (item === '0') {
+            this.date[index].active = false
+          } else {
+            this.date[index].active = true
+          }
+        })
+      }
+    },
+    hideeditorAlarm (bol) {
+      this.isShowEditorAlarm = false
+      const info = this.selectAlarm.alarm
+      // if (bol) {
+      //   if (!(info.alarmlockDate && info.alarmclockRemarks)) {
+      //     return Toast({
+      //       message: '闹钟信息不能为空',
+      //       iconClass: 'icon icon-error'
+      //     })
+      //   }
+      //   let alarmclockWeek = ''
+      //   let num = 0
+      //   this.date.forEach(item => {
+      //     if (item.active) {
+      //       alarmclockWeek += '1'
+      //       num += 1
+      //     } else {
+      //       alarmclockWeek += '0'
+      //     }
+      //   })
+      //   if (!num) {
+      //     alarmclockWeek = '1'
+      //   } else if (num === 7) {
+      //     alarmclockWeek = '2'
+      //   }
+      //   const data = {
+      //     alarmlockDate: info.alarmlockDate,
+      //     alarmclockWeek: alarmclockWeek,
+      //     alarmclockWearerId: 9611812844,
+      //     alarmclockRemarks: info.alarmclockRemarks,
+      //     alarmclockStatus: info.alarmclockStatus,
+      //     alarmclockId: info.alarmclockId
+      //     key: 'REMIND'
+      //   }
+      //   this.$http.post(`${config.httpBaseUrl}/alar/insert`, data).then(res => {
+      //     if (res.code === 200) {
+      //       this.alarms.push(data)
+      //     }
+      //   })
+      //   Toast({
+      //     message: '操作成功',
+      //     iconClass: 'icon icon-success'
+      //   })
+      // }
     },
     // 删除闹钟
     delAlarm () {
@@ -235,6 +326,10 @@ export default {
         message: '操作成功',
         iconClass: 'icon icon-success'
       })
+    },
+    // 改变闹钟状态
+    changeStatus (item, index) {
+      console.log(item, this.alarms[index])
     }
   }
 }
@@ -271,6 +366,11 @@ export default {
     }
   }
   .content {
+    overflow: auto;
+    position: fixed;
+    top: 1.44rem;
+    bottom: 0;
+    width: 100vw;
     .alarm_item {
       display: flex;
       padding: 0 .5rem;
